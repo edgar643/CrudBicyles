e5e1f959e1e084800ee093d5227b328594ac05e5
Edgar José <edgar.jose.garcia.l@gmail.com>
2023-10-25 20:52:09 -0500

    comment

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();//Load env 
+  dotenv.config();//Load enviorement variables
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
